dd93fa818538f762c5ba2754604ebd9ae1dc4e35
Samuel Mitterrutzner <sa.mitterrutzner@gmail.com>
2021-10-05 15:13:54 +0200

    adding function to get data from the csv

--- a1/vis.js
+++ a1/vis.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+const Props = {
+    state: "State",
+    gdp: "GDP"
+}
+
+const data = [];
+
+async function getData() {
+    const d = [];
+    await d3.csv("usa_nominal_gdp_top10_2021.csv", (rawData) => {
+        d.push(rawData);
+    })
+    console.log(d);
+    return d;
+}
+
+getData();
