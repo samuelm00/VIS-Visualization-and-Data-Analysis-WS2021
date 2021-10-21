e01686731ac66db15bb65c59b5ba265f1a631d90
Samuel Mitterrutzner <sa.mitterrutzner@gmail.com>
2021-10-21 18:40:26 +0200

    adding simple line charts

---
+++ a2/README.md
@@ -0,0 +1,8 @@
+Resources:
+- Documentation of D3: https://devdocs.io/d3~4/
+- Files from the D3 Tutorial
+
+What I did: 
+- created simple bar chart with d3
+- used the `range` method for both axis
+- added labels to the x and y axis by adding the text attribute to the `svg - Element`
---
+++ a2/vis.js
@@ -0,0 +1,116 @@
+const Props = {
+    state: "State"
+}
+
+/**
+ * Basic sizes and Elements
+ * @type {number}
+ */
+const margin = 100;
+const height = 600 - margin*2;
+const width = 1000 - margin*2;
+const svgElement = d3.select("#line-chart");
+const lineChart = svgElement.append("g").attr("transform", `translate(${margin}, ${margin})`);
+
+/**
+ *
+ * @returns {Promise<*[]>}
+ */
+async function getData() {
+    const d = [];
+    await d3.csv("usa_nominal_gdp_1997-2020.csv", (rawData) => {
+        d.push(rawData);
+    })
+    return d;
+}
+
+/**
+ *
+ * @param range
+ * @param data
+ * @param variant
+ * @returns {*}
+ */
+function calculateScale(range, data, variant) {
+    if (variant === "y") {
+        return d3.scaleLinear().range(range).domain([0, data[data.length-1]])
+    }
+    return d3.scaleBand().range(range).domain(data);
+}
+
+/**
+ *
+ * @param xAxis
+ * @param yAxis
+ * @param data
+ */
+function drawSingleLine(xAxis, yAxis, data) {
+    lineChart.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "blue")
+        .attr("stroke-width", 1)
+        .attr("d", d3.line()
+            .x(d => xAxis(d.year))
+            .y(d => yAxis(d.gdp))
+        );
+}
+
+/**
+ *
+ * @param xAxis
+ * @param yAxis
+ * @param data
+ */
+function drawLines(xAxis, yAxis, data) {
+    data.forEach(d => {
+        const dataArr = Object
+            .keys(d)
+            .filter(key => key !== Props.state)
+            .map(key => ({
+                year: key,
+                gdp: +d[key]
+            }))
+        drawSingleLine(xAxis, yAxis, dataArr)
+    })
+}
+
+(async function () {
+   const data = await getData();
+
+    /**
+     * Calculate scales
+     * @type {*}
+     */
+   const xScale = calculateScale([0, width], Object.keys(data[0]).filter(key => key !== Props.state));
+
+    const yDomain = [].concat(...data.map(d =>
+         Object
+            .keys(d)
+            .filter(key => key !== Props.state)
+            .map(key => Math.round(+d[key]))
+    )).sort((a,b) => a-b);
+
+   const yScale = calculateScale([height, 0], yDomain, "y");
+
+    /**
+     * Add Axiss
+     */
+    lineChart.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(xScale))
+
+    lineChart.append("g")
+        .call(d3.axisLeft(yScale))
+
+    drawLines(xScale, yScale, data)
+
+}())
+
+
+// const arr = Object
+//     .keys(d)
+//     .filter(key => key !== Props.state)
+//     .map(key => Math.round(+d[key]))
+// console.log(arr);
+// return arr;
