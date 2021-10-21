01ec67419e7af229cd53b11e9d395e373640dc04
Samuel Mitterrutzner <sa.mitterrutzner@gmail.com>
2021-10-21 22:31:40 +0200

    adding hover effekts

--- a2/vis.js
+++ a2/vis.js
@@ -1,5 +1,19 @@
+/**
+ *
+ * @type {{state: string}}
+ */
 const Props = {
     state: "State"
+}
+
+/**
+ *
+ * @type {{color: string, active: string, id: string}}
+ */
+const ActiveObjectProps = {
+    color: "color",
+    id: "id",
+    active: "active"
 }
 
 /**
@@ -7,7 +21,7 @@ const Props = {
  * @type {number}
  */
 const margin = 100;
-const height = 600 - margin*2;
+const height = 800 - margin*2;
 const width = 1000 - margin*2;
 const svgElement = d3.select("#line-chart");
 const lineChart = svgElement.append("g").attr("transform", `translate(${margin}, ${margin})`);
@@ -44,16 +58,20 @@ function calculateScale(range, data, variant) {
  * @param yAxis
  * @param data
  */
-function drawSingleLine(xAxis, yAxis, data) {
+function drawSingleLine(xAxis, yAxis, data, color) {
     lineChart.append("path")
         .datum(data)
         .attr("fill", "none")
-        .attr("stroke", "blue")
-        .attr("stroke-width", 1)
+        .attr("stroke", color)
+        .attr("stroke-width", 2)
         .attr("d", d3.line()
             .x(d => xAxis(new Date(d.year)))
             .y(d => yAxis(d.gdp))
-        );
+        )
+    }
+
+function getRandomNumber(maxNr) {
+    return Math.floor(Math.random() * maxNr);
 }
 
 /**
@@ -63,7 +81,7 @@ function drawSingleLine(xAxis, yAxis, data) {
  * @param data
  */
 function drawLines(xAxis, yAxis, data) {
-    data.forEach(d => {
+    data.forEach((d, i) => {
         const dataArr = Object
             .keys(d)
             .filter(key => key !== Props.state)
@@ -71,12 +89,25 @@ function drawLines(xAxis, yAxis, data) {
                 year: key,
                 gdp: +d[key]
             }))
-        drawSingleLine(xAxis, yAxis, dataArr)
+        drawSingleLine(xAxis, yAxis, dataArr,
+            `rgb(${getRandomNumber(255)+i}, ${getRandomNumber(255)+i}, ${getRandomNumber(255)+i})`)
     })
+}
+
+/**
+ *
+ * @param activeObject
+ * @param data
+ * @returns {*}
+ */
+function getActiveObject(activeObject, data) {
+    return activeObject.find(aO => aO[ActiveObjectProps.id] === data[Props.state])
 }
 
 (async function () {
    const data = await getData();
+   let activeObjects = []
+
     /**
      * Calculate scales
      * @type {*}
@@ -107,4 +138,64 @@ function drawLines(xAxis, yAxis, data) {
      */
     drawLines(xScale, yScale, data)
 
+    /**
+     * Add Labels
+     */
+    svgElement.append("text")
+        .attr("x", -(height / 2) - margin)
+        .attr("y", 20)
+        .attr("transform", "rotate(-90)")
+        .attr("text-anchor", "middle")
+        .text("Nominal GDP (millions of $)")
+
+    svgElement.append('text')
+        .attr("x", width / 2 + margin)
+        .attr("y", 750)
+        .attr("text-anchor", "middle")
+        .text("Years")
+
+    /**
+     * Adding hover effekts
+     */
+    lineChart.selectAll("path")
+        .data(data)
+        .on("mouseover", function (event, data) {
+            const activeObject = getActiveObject(activeObjects, data)
+            if (!activeObject) {
+                // save current color and the name of the state as id
+                activeObjects.push({
+                    [ActiveObjectProps.color]: d3.select(this).attr("stroke"),
+                    [ActiveObjectProps.id]: data[Props.state]
+                })
+            }
+
+            // change color to black
+            d3.select(this)
+                .attr("stroke", "black")
+                .attr("id", data[Props.state])
+
+            // add text to the path
+            lineChart.append("text")
+                .append("textPath")
+                .attr("xlink:href", `#${data[Props.state]}`)
+                .attr("id", `${data[Props.state]}-text`)
+                .attr("startOffset", "50%")
+                .text(data[Props.state])
+        })
+        .on("mouseout", function (event, data) {
+            const activeObject = getActiveObject(activeObjects, data);
+            if (!activeObject || !activeObject[ActiveObjectProps.active]) {
+                // remove text from line chart
+                lineChart.select(`#${data[Props.state]}-text`).remove()
+                // remove black color and add the old one
+                d3.select(this)
+                    .attr("stroke", activeObjects.find(aO => aO[ActiveObjectProps.id] === data[Props.state])[ActiveObjectProps.color])
+                    .attr("id", "")
+                activeObjects = activeObjects.filter(aO => aO[ActiveObjectProps.id] !== data[Props.state])
+            }
+        })
+        .on("click", function (event, data) {
+            const activeObject = getActiveObject(activeObjects, data);
+            if (activeObject)  activeObject[ActiveObjectProps.active] = !activeObject[ActiveObjectProps.active];
+        })
 }())
