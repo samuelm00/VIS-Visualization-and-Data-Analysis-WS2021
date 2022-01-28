ce59d83e69101e93f158df73ce8f1ed8f3a7a9ee
Samuel Mitterrutzner <sa.mitterrutzner@gmail.com>
2022-01-28 12:11:12 +0100

    adding simple year slider component

--- a5/a5/src/App.tsx
+++ a5/a5/src/App.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import PageLayout from "./components/Layout/PageLayout";
 import Header from "./components/Layout/Header";
 import WorldMap from "./components/WorldMap/WorldMap";
+import Slider from "./components/Slider/Slider";
 
 function App() {
+  const [currentYear, setCurrentYear] = React.useState(2006);
+
   return (
     <PageLayout>
       <Header variant={"page"}>COVID-19 Dashboard</Header>
       <WorldMap />
+      <Slider value={currentYear} setValue={setCurrentYear} />
     </PageLayout>
   );
 }
---
+++ a5/a5/src/components/Slider/Slider.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Slider from "rc-slider";
+
+interface YearSliderProps {
+  value: number;
+  setValue: React.Dispatch<React.SetStateAction<number>>;
+}
+
+export default function YearSlider({ value, setValue }: YearSliderProps) {
+  return (
+    <Slider
+      value={value}
+      onChange={(e) => setValue(e)}
+      min={2006}
+      max={2019}
+      step={1}
+      marks={{
+        2006: "2006",
+        2007: "2007",
+        2008: "2008",
+        2009: "2009",
+        2010: "2010",
+        2011: "2011",
+        2012: "2012",
+        2013: "2013",
+        2014: "2014",
+        2015: "2015",
+        2016: "2016",
+        2017: "2017",
+        2018: "2018",
+        2019: "2019",
+      }}
+    />
+  );
+}
