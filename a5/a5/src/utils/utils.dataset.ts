16ac1aabce16e7a9af34ba26a53e70c0f7cd0957
Samuel Mitterrutzner <sa.mitterrutzner@gmail.com>
2022-02-01 09:58:58 +0100

    changing base for deployment

--- a5/a5/src/utils/utils.dataset.ts
+++ a5/a5/src/utils/utils.dataset.ts
@@ -1,12 +1,10 @@
 import { json } from "d3";
 import { DataSetType, TimeDataMeasurements } from "../types/type.dataset";
 
-const BASE_PROD = "/~samuelm00/VIS21W/A5/data";
-const BASE_DEV = "/~samuelm00/VIS21W/A5";
+const BASE = "/~samuelm00/VIS21W/A5";
 
 export async function getDataset(): Promise<DataSetType[]> {
-  const base = window.location.hostname === "localhost" ? BASE_DEV : BASE_PROD;
-  const data: any = await json(`${base}/owid-covid-data.json`);
+  const data: any = await json(`${BASE}/owid-covid-data.json`);
   if (!data) throw Error("No data");
   const dataArr = Object.keys(data).map((key) => ({ ...data[key] }));
   return dataArr as DataSetType[];
