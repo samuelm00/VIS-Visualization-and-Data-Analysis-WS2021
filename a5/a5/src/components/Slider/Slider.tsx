8c838b380b6cec7c6f58c4287cb23c92b7c72251
Samuel Mitterrutzner <sa.mitterrutzner@gmail.com>
2022-02-01 11:33:32 +0100

    improved styling of the slider

--- a5/a5/src/components/Slider/Slider.tsx
+++ a5/a5/src/components/Slider/Slider.tsx
@@ -12,6 +12,21 @@ export default function YearSlider() {
       min={2020}
       max={2022}
       step={1}
+      activeDotStyle={{
+        borderColor: "white",
+        borderWidth: "2px",
+        borderStyle: "solid",
+      }}
+      trackStyle={{
+        backgroundColor: "white",
+        color: "white",
+      }}
+      handleStyle={{
+        borderColor: "white",
+        borderWidth: "2px",
+        backgroundColor: "rgb(0, 148, 133)",
+        borderStyle: "solid",
+      }}
       marks={{
         2020: "2020",
         2021: "2021",
